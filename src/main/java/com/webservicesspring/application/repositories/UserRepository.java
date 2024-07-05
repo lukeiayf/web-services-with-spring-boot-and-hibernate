ef3d54adf698e6315cd66b9397bd8bbe8df7e285
Lucas Gomes <lukas_g_s_@hotmail.com>
2024-07-05 17:59:24 -0300

    adds test configuration

---
+++ src/main/java/com/webservicesspring/application/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.webservicesspring.application.repositories;
+
+import com.webservicesspring.application.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+
+
+}
