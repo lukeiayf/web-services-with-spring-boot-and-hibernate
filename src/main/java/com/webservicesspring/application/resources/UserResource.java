0f1fddd8e5fa5e46f22666d6729a730f8a93d9be
Lucas Gomes <lukas_g_s_@hotmail.com>
2024-07-05 18:15:32 -0300

    implements find by id and find all methods

--- src/main/java/com/webservicesspring/application/resources/UserResource.java
+++ src/main/java/com/webservicesspring/application/resources/UserResource.java
@@ -1,19 +1,35 @@
 package com.webservicesspring.application.resources;
 
 import com.webservicesspring.application.entities.User;
+import com.webservicesspring.application.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+    @Autowired
+    private UserService service;
+
     @GetMapping
-    public ResponseEntity<User> findAll() {
-        User u = new User(1L, "MAria", "Maria@gmail.com", "12312312", "12345");
-        return ResponseEntity.ok().body(u);
+    public ResponseEntity<List<User>> findAll() {
+        List<User> users = service.findAll();
+
+        return ResponseEntity.ok().body(users);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        User user = service.findById(id);
+
+        return ResponseEntity.ok().body(user);
     }
 
 }
