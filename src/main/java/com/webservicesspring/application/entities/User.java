063fe2e9528eb93b161f7f166cf39a95a068b10c
Lucas Gomes <lukas_g_s_@hotmail.com>
2024-07-10 16:16:48 -0300

    adds order entity

--- src/main/java/com/webservicesspring/application/entities/User.java
+++ src/main/java/com/webservicesspring/application/entities/User.java
@@ -3,6 +3,8 @@ package com.webservicesspring.application.entities;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
@@ -15,6 +17,8 @@ public class User implements Serializable {
     private String email;
     private String phone;
     private String password;
+    @OneToMany(mappedBy = "client")
+    private final List<Order> orders = new ArrayList<>();
 
     public User() {
     }
@@ -65,6 +69,10 @@ public class User implements Serializable {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
     }
 
     @Override
