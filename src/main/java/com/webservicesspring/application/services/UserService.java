4ec51d023699d2782850c8ea06ee1aa9303f5f62
Lucas Gomes <lukas_g_s_@hotmail.com>
2024-07-30 16:57:56 -0300

    feat: add exception handling for delete mapping in user service

--- src/main/java/com/webservicesspring/application/services/UserService.java
+++ src/main/java/com/webservicesspring/application/services/UserService.java
@@ -2,8 +2,11 @@ package com.webservicesspring.application.services;
 
 import com.webservicesspring.application.entities.User;
 import com.webservicesspring.application.repositories.UserRepository;
+import com.webservicesspring.application.services.exceptions.DatabaseException;
 import com.webservicesspring.application.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -30,7 +33,13 @@ public class UserService {
     }
 
     public void delete(Long id) {
-        repository.deleteById(id);
+        try {
+            repository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
     }
 
     public User update(Long id, User obj) {
